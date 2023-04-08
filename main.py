2e87aee36e68868b15238e65f0ea509153420cab
ca-ye <112222171+ca-ye@users.noreply.github.com>
2023-04-08 14:29:19 +1000

    updated code 8/4/23

--- main.py
+++ main.py
@@ -8,10 +8,26 @@ from time import sleep
 
 factory = PiGPIOFactory(host='fe80::584a:22e2:4eff:931f%4')  # Connects to the pi, host is the ip
 
-servo_1 = Servo(21, pin_factory=factory)    # left strike
-servo_2 = Servo(20, pin_factory=factory)    # right angle
-servo_3 = Servo(26, pin_factory=factory)    # right strike
-servo_4 = Servo(19, pin_factory=factory)    # left angle
+servo_1 = Servo(21, pin_factory=factory)  # left strike
+servo_2 = Servo(20, pin_factory=factory)  # right angle
+servo_3 = Servo(26, pin_factory=factory)  # right strike
+servo_4 = Servo(19, pin_factory=factory)  # left angle
+
+servo_1.value = 0
+servo_2.value = 0
+servo_3.value = 0
+servo_4.value = 0
+
+keys = {
+    "C": 1,
+    "D": 2,
+    "E": 3,
+    "F": 4,
+    "G": 5,
+    "A": 6,
+    "B": 7,
+    "C\'": 8
+}
 
 def turn_on_servos(event):
     if event == '1':
@@ -21,71 +37,78 @@ def turn_on_servos(event):
         sleep(0.5)
         servo_1.value = 0
         sleep(0.5)
+
     elif event == '2':
         servo_4.value = 0.1
         sleep(0.5)
         servo_1.value = 0.7
         sleep(0.5)
-        servo_4.value = 0
         servo_1.value = 0
         sleep(0.5)
+
     elif event == '3':
         servo_4.value = -0.05
         sleep(0.5)
         servo_1.value = 0.7
         sleep(0.5)
-        servo_4.value = 0
         servo_1.value = 0
         sleep(0.5)
+
     elif event == '4':
         servo_4.value = -0.25
         sleep(0.5)
         servo_1.value = 0.7
         sleep(0.5)
-        servo_4.value = 0
         servo_1.value = 0
         sleep(0.5)
+
     elif event == '5':
         servo_2.value = 0.1
         sleep(0.5)
         servo_3.value = 0.7
         sleep(0.5)
-        servo_2.value = 0
         servo_3.value = 0
         sleep(0.5)
+
     elif event == '6':
         servo_2.value = -0.1
         sleep(0.5)
         servo_3.value = 0.7
         sleep(0.5)
-        servo_2.value = 0
         servo_3.value = 0
         sleep(0.5)
+
     elif event == '7':
         servo_2.value = -0.25
         sleep(0.7)
         servo_3.value = 0.7
         sleep(0.5)
-        servo_2.value = 0
         servo_3.value = 0
         sleep(0.5)
+
     elif event == '8':
         servo_2.value = -0.4
         sleep(0.7)
         servo_3.value = 0.7
         sleep(0.5)
-        servo_2.value = 0
         servo_3.value = 0
         sleep(0.5)
+
+    elif event == 'submit':
+        sequence = values['sequence']
+        sequence = sequence.split(" ")
+        for i in sequence:
+            turn_on_servos(str(keys[i]))
+
 
 
 sg.theme('DarkBlue6')  # Add a touch of color
 # All the stuff inside your window.
 layout = [[sg.Text('Click on the letters to go to that letter on the xylophone.')],
-          [sg.Text('Enter a sequence'), sg.InputText()],
+          [sg.Text('Enter a sequence'), sg.InputText(key='sequence')],
           [sg.Button('C', key='1'), sg.Button('D', key='2'), sg.Button('E', key='3'), sg.Button('F', key='4'),
            sg.Button('G', key='5'), sg.Button('A', key='6'),
-           sg.Button('B', key='7'), sg.Button('C', key='8'), sg.Button('Rest')]]
+           sg.Button('B', key='7'), sg.Button('C\'', key='8'), sg.Button('Enter Sequence', key='submit')]]
 
 window = sg.Window('XYLOPHONER', layout)
 
@@ -93,9 +116,11 @@ window = sg.Window('XYLOPHONER', layout)
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        servo_1.value = 0
+        servo_2.value = 0
+        servo_3.value = 0
+        servo_4.value = 0
         break
-
-    print('You entered', event)
     turn_on_servos(event)
 
 window.close()
